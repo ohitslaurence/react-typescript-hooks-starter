03f43e4ee4e63074abe5eb21b48caa036da33eb5
ljdavies <laurence.davies@libryo.com>
2020-04-08 15:36:26 +0200

    Remove .env from git

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,4 @@
 // postcss.config.js
-// const purgecss = require('@fullhuman/postcss-purgecss')({
-//   // Specify the paths to all of the template files in your project
-//   content: ['./src/**/*.js', './src/**/*.ts', './src/**/*.tsx', './public/index.html'],
-
-//   // Include any special characters you're using in this regular expression
-//   defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-// });
 const purgecss = require('@fullhuman/postcss-purgecss');
 
 const tailwindcss = require('tailwindcss');
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import './translation';
 import App from './App';
 
 test('renders learn react link', () => {
