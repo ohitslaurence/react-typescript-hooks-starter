3fde0d861ec2256004c51195f5251625d254b667
ljdavies <laurence.davies@libryo.com>
2020-04-08 14:31:32 +0200

    Fix production build

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,12 @@
 // postcss.config.js
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  // Specify the paths to all of the template files in your project
-  content: ['./src/**/*.js', './src/**/*.ts', './src/**/*.tsx', './public/index.html'],
+// const purgecss = require('@fullhuman/postcss-purgecss')({
+//   // Specify the paths to all of the template files in your project
+//   content: ['./src/**/*.js', './src/**/*.ts', './src/**/*.tsx', './public/index.html'],
 
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-});
+//   // Include any special characters you're using in this regular expression
+//   defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+// });
+const purgecss = require('@fullhuman/postcss-purgecss');
 
 const tailwindcss = require('tailwindcss');
 
@@ -14,6 +15,12 @@ module.exports = {
     require('postcss-import'),
     tailwindcss('./src/tailwind.js'),
     require('autoprefixer'),
-    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+    ...(process.env.NODE_ENV === 'production'
+      ? [
+          purgecss({
+            content: ['./src/**/*.html', './src/**/*.tsx', './src/**/*.ts'],
+          }),
+        ]
+      : []),
   ],
 };
